07dde6725805dd0ab8ce71347aff3026e9ca95ce
zhangyongbin <18810912129@163.com>
2018-09-10 15:57:44 +0800

    fisrt commit for onlinestore-manager Architecture

---
+++ repo-onlinestore-manager/onlinestore-manager-service/src/main/java/com/zhangyongbin/onlinestore/App.java
@@ -0,0 +1,13 @@
+package com.zhangyongbin.onlinestore;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/zhangyongbin/onlinestore/RepoOnlinestoreParentApplication.java
@@ -0,0 +1,12 @@
+package com.zhangyongbin.onlinestore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoOnlinestoreParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoOnlinestoreParentApplication.class, args);
+	}
+}
